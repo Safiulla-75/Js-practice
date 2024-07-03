55964490b4be3a56ea69c66eb68572c18a3470bf
safiulla <safullask75@gmail.com>
2024-07-03 15:28:12 +0530

    added js practice code

---
+++ script.js
@@ -0,0 +1,79 @@
+//alert("this is my first code");
+
+// let money = 100;
+// console.log(money);
+
+// let money100 = 45;
+// console.log(money100);
+// money100 = money + 40;
+// console.log(money100);
+
+// console.log(".............................................")
+// let noOfStudent = 40;
+// console.log(noOfStudent)
+// noOfStudent = noOfStudent+5;
+// console.log(noOfStudent)
+
+//use of const.............................................................................;
+// let radius = 4;
+// const pi = 3.14;
+// let area = pi * radius;
+// console.log(area)
+
+// pi = 75;
+// let perimeter= 2*pi*radius;
+// console.log(perimeter);
+
+//string...........................................................................;
+// let str1 = "This is first str";
+// console.log(str1);
+// let str2 = 'This is second str';
+// console.log(str2);
+// noOfStr = 10000
+// let str3 = `str3 use ${noOfStr} `;
+// let str4 = 'str3 use'+ noOfStr;
+// console.log(str3);
+// console.log(str3);
+
+//Boolean.....................................................;
+// let isAdut = true;//this is boolean
+// let isAdut2 = 'true';// this is string;
+// console.log(isAdut);
+// console.log(isAdut2);
+
+// null and undefined ..............................................;
+// null এর মধ্যে কোনো value define নেই । আর undefined এর মধ্যে ভেরিয়েবল থাকে কিন্তু কোনো value দেওয়া থাকে না .  
+// var userName;
+
+// console.log(`apnar ami jani na ${userName}`);
+// userName = 'Abdullah';
+// console.log(`apnar name ${userName}`);
+
+//typeof operator..........................;
+
+// console.log(typeof 4);
+// console.log(`type of 4 is ${typeof 4}`);
+// console.log(`type of hello is ${typeof 'hello'}`);
+// console.log(`type of 4.5 is ${typeof 4.5}`);
+// console.log(`type of true is ${typeof true}`);
+// console.log(`type of false is ${typeof false}`);
+// console.log(`type of null is ${typeof null}`);
+// var userName;
+// console.log(`type of userName is ${typeof undefined}`);
+
+//Comarison Operator (==, ===, !=, !==, >, <, >=, <=)......................;
+// console.log(4 == 4);
+// console.log(4 == 4.0);
+// console.log(4 === 4);
+// console.log(4 === 4.0);
+// console.log(4 !== 4);
+
+// console.log('Hello' == "Hello")
+// console.log('Hello' === "Hello")
+// console.log("Hello" == "Hello")
+// console.log('Hello' == "hello")
+// console.log('Hello' === "hello")
+
+// console.log(4 > 5);
+// console.log(4 < 5);
+
